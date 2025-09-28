f62977164a2e9035ed40ec904ef915f3b9b0d96d
Nawal <nawal.mhamed@gmail.com>
2025-09-28 14:49:57 +0200

    Adding missing adjustments

--- src/pages/LegalNotices.jsx
+++ src/pages/LegalNotices.jsx
@@ -38,20 +38,7 @@ const LegalNotices = () => {
               <div class="accordion-body">
                 <p class="h4">John Doe</p>
                 <p>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-map"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M15.817.113A.5.5 0 0 1 16 .5v14a.5.5 0 0 1-.402.49l-5 1a.5.5 0 0 1-.196 0L5.5 15.01l-4.902.98A.5.5 0 0 1 0 15.5v-14a.5.5 0 0 1 .402-.49l5-1a.5.5 0 0 1 .196 0L10.5.99l4.902-.98a.5.5 0 0 1 .415.103M10 1.91l-4-.8v12.98l4 .8zm1 12.98 4-.8V1.11l-4 .8zm-6-.8V1.11l-4 .8v12.98z"
-                    />
-                  </svg>{" "}
-                  40 rue Laure Diebold <br />
+                  <i class="bi bi-map"></i>40 rue Laure Diebold <br />
                   <i class="bi bi-geo-alt"></i> 69009 Lyon, France <br />
                   <i class="bi bi-phone"></i> 10 20 30 40 50 <br />
                   <i class="bi bi-envelope-at"></i> john.doe@gmail.com
